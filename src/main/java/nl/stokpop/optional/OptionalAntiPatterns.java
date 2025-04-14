72aabb54363a503f4713f3b71a7a7e14c8dc5f2d
Peter Paul Bakker <peterpaul@perfana.io>
2025-04-14 16:56:15 +0200

    update OptionalAntiPatterns

--- src/main/java/nl/stokpop/optional/OptionalAntiPatterns.java
+++ src/main/java/nl/stokpop/optional/OptionalAntiPatterns.java
@@ -1,20 +1,36 @@
 package nl.stokpop.optional;
 
+import java.math.BigDecimal;
 import java.util.Optional;
+import java.util.Random;
 
 public class OptionalAntiPatterns {
 
+    private static Random random = new Random(System.currentTimeMillis());
+
     public static void main(String[] args) {
-        Optional<String> value = Optional.ofNullable("thisIsNotNull");
-        System.out.println("Value is present: " + value.orElse(getFromRemoteService()));
+        Optional<BigDecimal> value = Optional.ofNullable(calculateValue(0.10));
+        BigDecimal finalValue;
+        if (value.isPresent()) {
+            finalValue = value.get();
+        } else {
+            finalValue = longRemoteCallForValue();
+        }
+        System.out.println("Final value: " + finalValue);
     }
 
-    private static String getFromRemoteService() {
+    private static BigDecimal calculateValue(double val) {
+        return random.nextInt() % 2 == 0 ? new BigDecimal(val) : null;
+    }
+
+    private static BigDecimal longRemoteCallForValue() {
+        // Simulate a long remote call
+        System.out.println("Calling long remote service...");
         try {
-            Thread.sleep(10_000);
+            Thread.sleep(5000); // Simulate delay
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
         }
-        return "thisIsFromRemoteService";
+        return new BigDecimal("0.00"); // Default value to simulate remote result
     }
 }
