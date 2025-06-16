d9eb1351b190718fc06366d7bf47a410e1919501
Peter Paul Bakker <peter.paul.bakker@stokpop.nl>
2025-06-16 13:07:50 +0200

    most simple TestVolatile for demo

--- src/main/java/nl/stokpop/concurrency/TestVolatile.java
+++ src/main/java/nl/stokpop/concurrency/TestVolatile.java
@@ -6,32 +6,41 @@ import java.util.Map;
 class TestVolatile extends Thread {
 
     //volatile
-    private volatile Map<String, Boolean> keepRunning = Collections.emptyMap();
+    //private volatile Map<String, Boolean> keepRunning = Collections.emptyMap();
     // private Map<String, Boolean> keepRunning = Collections.emptyMap();
 
     //volatile Boolean keepRunning = Boolean.TRUE;
     //Boolean keepRunning = Boolean.TRUE;
-    //volatile boolean keepRunning = true;
-    // boolean keepRunning = true;
+
+    volatile boolean keepRunning = true;
+    //boolean keepRunning = true;
 
     public void run() {
         long count = 0;
 
-        while (keepRunning()) {
+        while (keepRunning) {
             count++;
         }
 
         System.out.println("Thread terminated. Count: " + count);
     }
 
-    public boolean keepRunning() {
-        return !keepRunning.containsKey("TRUE");
-    }
+//    public boolean keepRunning() {
+//        return !keepRunning.containsKey("TRUE");
+//    }
+//    public boolean keepRunning() {
+//        return !keepRunning;
+//    }
 
-    public void stopRunning() {
-        // this.keepRunning = Boolean.FALSE;
-        this.keepRunning = Collections.singletonMap("TRUE", Boolean.TRUE);
-    }
+//    public void stopRunning() {
+//        // this.keepRunning = Boolean.FALSE;
+//        this.keepRunning = Collections.singletonMap("TRUE", Boolean.TRUE);
+//    }
+
+//    public void stopRunning() {
+//        // this.keepRunning = Boolean.FALSE;
+//        this.keepRunning = false;
+//    }
 
     public static void main(String[] args) throws InterruptedException {
 
@@ -42,8 +51,8 @@ class TestVolatile extends Thread {
 
         System.out.println("After sleeping in main, request thread to stop.");
 
-        //t.keepRunning = Boolean.FALSE;
-        t.stopRunning();
+        t.keepRunning = false;
+        //t.stopRunning();
         t.join();
 
         System.out.println("KeepRunning set to " + t.keepRunning);
