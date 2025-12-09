9eaabbb0ec2e75670cd4066bbedccc505afc3810
Peter Paul Bakker <peter.paul.bakker@stokpop.nl>
2025-12-09 09:36:39 +0100

    small code cleanup

--- src/main/java/nl/stokpop/circuitbreaker/CircuitBreakerFactory.java
+++ src/main/java/nl/stokpop/circuitbreaker/CircuitBreakerFactory.java
@@ -2,16 +2,18 @@ package nl.stokpop.circuitbreaker;
 
 import io.github.resilience4j.circuitbreaker.CircuitBreaker;
 import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import org.jetbrains.annotations.NotNull;
+
 import java.time.Duration;
 
 public class CircuitBreakerFactory {
-    
+
     public static CircuitBreaker createPaymentCircuitBreaker() {
         CircuitBreakerConfig config = CircuitBreakerConfig.custom()
             // Use COUNT_BASED for more predictable demo behavior
             .slidingWindowType(CircuitBreakerConfig.SlidingWindowType.COUNT_BASED)
             .slidingWindowSize(15) // Track last 15 calls (instead of time window)
-            .minimumNumberOfCalls(5) // ⚡ Reduced from 10 to 5 for faster demo
+            .minimumNumberOfCalls(5) // Reduced from 10 to 5 for faster demo
             .failureRateThreshold(50.0f) // 50% failure rate
             .slowCallRateThreshold(100.0f) // 100% slow calls trigger opening
             .slowCallDurationThreshold(Duration.ofSeconds(2)) // 2 seconds = slow
